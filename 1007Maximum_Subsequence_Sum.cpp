aa498c95e4284b5b230fa1ac84192ef4f3c333d0
Wind Lv <kaze_lvzf@outlook.com>
2019-04-10 19:13:38 +0800

    1007 complete,brute force

--- 1007Maximum_Subsequence_Sum.cpp
+++ 1007Maximum_Subsequence_Sum.cpp
@@ -3,43 +3,70 @@
 
 using namespace std;
 
-vector<int>::iterator findFirstPositive(vector<int>::iterator i_begin,vector<int>::iterator i_end){
-    while(i_begin!=i_end){
-        if(*i_begin > 0) break;
-        i_begin++;
-    }
-    return i_begin;
+struct SubSum {
+	int begin;
+	int end;
+	int sum;
+};
+
+int main() {
+	int k;
+	cin >> k;
+
+	vector<int> vec_num;
+	for (int i = 0; i < k; i++) {
+		int num;
+		cin >> num;
+		vec_num.push_back(num);
+	}
+
+	vector<int> vec_posIndex;
+	if (vec_num[0] > 0) vec_posIndex.push_back(0);
+	for (int i = 1; i < vec_num.size(); i++) 
+		if ((vec_num[i] >= 0) && (vec_num[i - 1] < 0)) vec_posIndex.push_back(i);        //find the begin of every subsequence whose elements are all positive
+
+	vector<SubSum> vec_sum;
+	if (vec_posIndex.empty()) {                           //if there is no positive number in sequence,add a extra case to vec_sum to output
+		SubSum extra{ 0,vec_num.size() - 1,0 };
+		vec_sum.push_back(extra);
+	}
+	for (int j = 0; j < vec_posIndex.size(); j++) {
+		int index = vec_posIndex[j];
+		int i = index;
+		int maxEnd = index;
+		int sum = vec_num[i++];
+		int maxSum = sum;
+		while (i < vec_num.size()) {                        //for every head of positive number, find their max subsequence that begins from themselfs
+			sum += vec_num[i];
+			if (sum > maxSum) {
+				maxSum = sum;
+				maxEnd = i;
+			}
+			i++;
+		}
+		SubSum subSum{ index,maxEnd,maxSum };
+		vec_sum.push_back(subSum);
+	}
+
+	int maxSubBegin = vec_sum[0].begin;
+	int maxSubEnd = vec_sum[0].end;
+	int maxSubSum = vec_sum[0].sum;
+	for (int i = 1; i < vec_sum.size(); i++) {               //pick the max subsequence of all
+		if (vec_sum[i].sum > maxSubSum) {
+			maxSubBegin = vec_sum[i].begin;
+			maxSubEnd = vec_sum[i].end;
+			maxSubSum = vec_sum[i].sum;
+		}
+	}
+	cout << maxSubSum << " " << vec_num[maxSubBegin] << " " << vec_num[maxSubEnd] << endl;
+
+	
+	return 0;
 }
 
-int main(){
-    int k;
-    cin>>k;
-
-    vector<int> vec_num;
-    for(int i = 0;i<k;i++){
-        int num;
-        cin>>num;
-        vec_num.push_back(num);
-    }
-
-    vector<int>::iterator i_first = vec_num.begin();
-    vector<int>::iterator i_last = vec_num.end()--;
-    int maxSum = 0;
-
-    auto i = i_first;
-    i =findFirstPositive(i,vec_num.end());
-    if(i!=vec_num.end()){
-        i_first = i;
-        i_last = i_first;
-        auto j = findFirstPositive(i_last,vec_num.end());
-        int sum = 0;
-        for(auto m = i_last+1;m!=j+1;m++) sum += *m;
-        if(sum>0) i_last = j;
-    }
-    int sum = 0;
-    for(auto j = i_first;j!=(i+1);j++) sum+=*j;
-    if(sum>0) i_last = i;
-
-    cout<<maxSum<<" "<<*i_first<<" "<<*i_last<<endl;
-    return 0;
-}
+/*
+i guess there is better approach to deal with this kind of question.
+i've tried three different stratege,but failed to find a more efficient way.
+spend too much time.And this is a brute force algorithm with time complexity O(n^2).
+by the way, be careful of edge conditions
+*/
