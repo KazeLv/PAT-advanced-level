91f5ee808a01aaa591347ad02928ecfe9e3842d4
Wind Lv <kaze_lvzf@outlook.com>
2019-04-15 21:43:38 +0800

    1010 complete! danm overflow!

--- 1010Radix.cpp
+++ 1010Radix.cpp
@@ -4,9 +4,9 @@
 
 using namespace std;
 
-int getValueInDecimal(string &digits, int radix)
+long long getValueInDecimal(string &digits, long long radix)
 {
-	int res = 0;
+	long long res = 0;
 	for (string::size_type i = 0; i < digits.size(); i++)
 	{
 		char curDigit = digits[digits.size() - 1 - i];
@@ -16,46 +16,48 @@ int getValueInDecimal(string &digits, int radix)
 	return res;
 }
 
-int getRadixByDecimal(string &digits, int value)
+long long getRadixByDecimal(string &digits, long long value)
 {
 	char maxDigit = digits[0];											//find the max number of all digits
 	for (string::size_type i = 1; i < digits.size(); i++)								//
 		if (digits[i] > maxDigit)										//
 			maxDigit = digits[i];										//
 	char cZero = (maxDigit >= '0' && maxDigit <= '9') ? '0' : 'a' - 10; //the max digit plus one is the minimum of possible radixes
-	int radix = maxDigit - cZero;										//
+	long long radix = maxDigit - cZero + 1;									//
+	if(radix<2) radix = 2;
 
-	if (getValueInDecimal(digits, radix) > value) 						//smallest radix will get a bigger result ,impossible
-		return -1;
-
-	int left = radix + 1;												//find the radix by binary search
-	int right = (value > 1 ? value : 1) + 1;
-	int mid;
+	bool flag = false;
+	long long left = radix;													//find the radix by binary search
+	long long right = (value > 1 ? value : 1) + 1;
+	long long mid;
 	while(left<=right) {
 		mid = (left + right) / 2;
-		int tmp = getValueInDecimal(digits, mid);
-		if(tmp == value)
+		long long tmp = getValueInDecimal(digits, mid);
+		if(tmp == value){
+			flag = true;
 			break;
+		}
 		else if (tmp>value||tmp<0)
 			right = mid - 1;
 		else
 			left = mid + 1;
 	}
-	return mid;
+	return flag ? mid : -1;
 }
 
 int main()
 {
 	string n1, n2;
-	int tag, radix_told;
+	int tag;
+	long long radix_told;
 	cin >> n1 >> n2 >> tag >> radix_told;
 
 	string value_told = (tag == 1 ? n1 : n2);
 	string value_another = (tag == 1 ? n2 : n1);
 
-	int value_10 = getValueInDecimal(value_told, radix_told);
+	long long value_10 = getValueInDecimal(value_told, radix_told);
 
-	int radix_another = getRadixByDecimal(value_another, value_10);
+	long long radix_another = getRadixByDecimal(value_another, value_10);
 	if (radix_another == -1)
 	{
 		cout << "Impossible" << endl;
@@ -66,3 +68,9 @@ int main()
 	}
 	return 0;
 }
+
+/*
+remember two things:
+1. if report a message of time over, change search algorithm.(binary search, for example)
+2. any question about numeric calculation, use long long!!!! or you will get ERROR because of overflow!!!
+*/
