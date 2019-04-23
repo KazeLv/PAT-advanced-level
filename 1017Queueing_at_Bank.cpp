58604c8db7e6fc0b4121ca86b2eabdc858e8bfbf
Wind Lv <kaze_lvzf@outlook.com>
2019-04-23 16:07:33 +0800

    1017 complete! new way to solve simulation problem

--- 1017Queueing_at_Bank.cpp
+++ 1017Queueing_at_Bank.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 struct Customer{
 	int arriveSec;
-	int pSec = 1;
+	int serveSec;
 };
 
 int main(){
@@ -19,50 +19,39 @@ int main(){
 	for (int i = 0; i < n; i++){
 		Customer cTmp;
 		int h, m, s;
-		scanf("%d:%d:%d %d", &h, &m, &s, &cTmp.pSec);
+		scanf("%d:%d:%d %d", &h, &m, &s, &cTmp.serveSec);
 		cTmp.arriveSec = h * 3600 + m * 60 + s;
-		cTmp.pSec *= 60;
+		cTmp.serveSec *= 60;
 		if(cTmp.arriveSec > (17*3600))		//customers who arrive after 17:00:00 will not be served and count
 			continue;
 		vec_customer.push_back(cTmp);
 	}
-	sort(vec_customer.begin(), vec_customer.end(), [](Customer &a, Customer &b) { return a.arriveSec > b.arriveSec; });
-	int servedN = vec_customer.size();
-
-	int waitTotal = 0;
-	int sNow = 8 * 3600;
-	int emptyWin = k;
-	while(sNow <= (17 * 3600))
-	{
-		while (emptyWin > 0)
-		{
-			if (vec_customer.size() - 1 - k + emptyWin < 0)
-				break;
-			Customer &cTmp = vec_customer[vec_customer.size() - 1 - k + emptyWin]; //get the customer who is new to be served
-			waitTotal += sNow - cTmp.arriveSec;									   //add his/her waiting time to total counter
-			emptyWin--;
-		}
-
-		for (int i = 0; i < k; i++)
-		{
-			if (vec_customer.size() - 1 - i < 0)
-				break;
-
-			Customer &cTmp = vec_customer[vec_customer.size() - 1 - i];
-			cTmp.pSec--;
-			if (cTmp.pSec == 0)
-			{																//if service is finished,
-				emptyWin++;													//count of empty window will increase
-				vector<Customer>::iterator it = vec_customer.end() - 1 - i; //end erase this customer
-				vec_customer.erase(it);										//
+	sort(vec_customer.begin(), vec_customer.end(), [](Customer &a, Customer &b) { return a.arriveSec < b.arriveSec; });
+	vector<int> vec_window_finish(k, 3600 * 8);
+	double waitTotal = 0.0;
+	for (int i = 0; i < vec_customer.size(); i++){
+		int iTmp = 0, minFinish = vec_window_finish[0];
+		for (int j = 0; j < vec_window_finish.size(); j++){
+			if(vec_window_finish[j]<minFinish){
+				iTmp = j;
+				minFinish = vec_window_finish[j];
 			}
 		}
-
-		sNow++;
+		
+		if(vec_customer[i].arriveSec >= vec_window_finish[iTmp]){
+			vec_window_finish[iTmp] = vec_customer[i].arriveSec + vec_customer[i].serveSec;
+		}else{
+			waitTotal += vec_window_finish[iTmp] - vec_customer[i].arriveSec;
+			vec_window_finish[iTmp] += vec_customer[i].serveSec;
+		}
 	}
 
-	if(servedN == 0)
+	if (vec_customer.size() == 0)
 		cout << "0.0" << endl;
 	else
-		cout << setiosflags(ostream::fixed) << setprecision(1) << waitTotal / 60.0 / servedN << endl;
+		cout << setiosflags(ostream::fixed) << setprecision(1) << waitTotal / 60 / vec_customer.size() << endl;
 }
+
+/*
+got a new way to solve simulation problem 
+*/
