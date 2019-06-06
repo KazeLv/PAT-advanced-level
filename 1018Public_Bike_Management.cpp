b3f6c184df3912ae843c3043036ac370fd269175
Wind Lv <kaze_lvzf@outlook.com>
2019-06-06 23:27:38 +0800

    1018 step1

--- 1018Public_Bike_Management.cpp
+++ 1018Public_Bike_Management.cpp
@@ -10,16 +10,18 @@ struct Path{
 	int needBike;
 };
 
+int capacity, nStations, nRoads, sp;
+int ppnMap[501][501];
+int pnBikes[501];
+int shortestLen = gc_inf;
+vector<vector<int>> vec_shortestWays;
+
 int main(){
-	int capacity,nStations,nRoads,sp;
 	cin >> capacity >> nStations >> nRoads >> sp;
 
-	int *pnBikes = new int[nStations + 1];
-	int **ppnMap = new int *[nStations + 1];
 	for (int i = 0; i <= nStations; i++)
 	{
 		cin >> pnBikes[i];
-		ppnMap[i] = new int[nStations + 1];
 		fill(ppnMap[i], ppnMap[i] + nStations, gc_inf);
 	}
 
@@ -30,142 +32,31 @@ int main(){
 	}
 
 	vector<bool> vec_visit(nStations + 1, false);
-	vector<int> vec_len(nStations + 1, gc_inf);
+	vector<int> vec_path;
 
-	vec_len[0] = 0;
-	for (int j = 0; j <= nStations;j++){
-		int index = -1, minLen = gc_inf;
-		for (int i = 0; i <= nStations; i++)
-		{
-			if (vec_len[i] < minLen && vec_visit[i] == false)
-			{
-				minLen = vec_len[i];
-				index = i;
-			}
-		}
-		if(index = -1)
-			break;
+	vec_visit[0] = true;
+	vec_path.push_back(0);
+	int len = 0;
 
-		vec_visit[index] = true;
-		for (int i = 0; i <= nStations; i++)
-		{
-			if (vec_len[index] + ppnMap[index][i] < vec_len[i] && vec_visit[i] == false)
-			{
-				vec_len[i] == vec_len[index] + ppnMap[index][i];
-			}
-		}
-	}	
+	BFS(vec_visit, vec_path, 0);
 }
 
-//version liuchou
+void BFS(vector<bool> vec_visit, vector<int> vec_path, int len){
+	int addr = vec_path.back();
+	for (int i = 0; i <= nStations;i++){
+		if(vec_visit[i] == false && ppnMap[addr][i] != gc_inf){
+			len += ppnMap[addr][i];
+			if(len < shortestLen){
+				if(i == sp){
+					vec_path.push_back(i);
+					
+				}else{
 
-#include <iostream>
-#include <algorithm>
-#include <vector>
-using namespace std;
-const int inf = 99999999;
-int cmax, n, sp, m;
-int minNeed = inf, minBack = inf;
-int e[510][510], dis[510], weight[510];
-bool visit[510];
-vector<int> pre[510], path, temppath;
-void dfs(int v)
-{
-	temppath.push_back(v);
-	if (v == 0)
-	{
-		int need = 0, back = 0;
-		for (int i = temppath.size() - 1; i >= 0; i--)
-		{
-			int id = temppath[i];
-			if (weight[id] > 0)
-			{
-				back += weight[id];
-			}
-			else
-			{
-				if (back > (0 - weight[id]))
-				{
-					back += weight[id];
-				}
-				else
-				{
-					need += ((0 - weight[id]) - back);
-					back = 0;
 				}
 			}
-		}
-		if (need < minNeed)
-		{
-			minNeed = need;
-			minBack = back;
-			path = temppath;
-		}
-		else if (need == minNeed && back < minBack)
-		{
-			minBack = back;
-			path = temppath;
-		}
-		temppath.pop_back();
-		return;
-	}
-	for (int i = 0; i < pre[v].size(); i++)
-		dfs(pre[v][i]);
-	temppath.pop_back();
-}
-int main()
-{
-	fill(e[0], e[0] + 510 * 510, inf);
-	fill(dis, dis + 510, inf);
-	scanf("%d%d%d%d", &cmax, &n, &sp, &m);
-	for (int i = 1; i <= n; i++)
-	{
-		scanf("%d", &weight[i]);
-		weight[i] = weight[i] - cmax / 2;
-	}
-	for (int i = 0; i < m; i++)
-	{
-		int a, b;
-		scanf("%d%d", &a, &b);
-		scanf("%d", &e[a][b]);
-		e[b][a] = e[a][b];
-	}
-	dis[0] = 0;
-	for (int i = 0; i <= n; i++)
-	{
-		int u = -1, minn = inf;
-		for (int j = 0; j <= n; j++)
-		{
-			if (visit[j] == false && dis[j] < minn)
-			{
-				u = j;
-				minn = dis[j];
-			}
-		}
-		if (u == -1)
-			break;
-		visit[u] = true;
-		for (int v = 0; v <= n; v++)
-		{
-			if (visit[v] == false && e[u][v] != inf)
-			{
-				if (dis[v] > dis[u] + e[u][v])
-				{
-					dis[v] = dis[u] + e[u][v];
-					pre[v].clear();
-					pre[v].push_back(u);
-				}
-				else if (dis[v] == dis[u] + e[u][v])
-				{
-					pre[v].push_back(u);
-				}
+			if(len == shortestLen && i == sp){
+
 			}
 		}
 	}
-	dfs(sp);
-	printf("%d 0", minNeed);
-	for (int i = path.size() - 2; i >= 0; i--)
-		printf("->%d", path[i]);
-	printf(" %d", minBack);
-	return 0;
 }
