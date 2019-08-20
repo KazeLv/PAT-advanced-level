93a805dbb790abad484f36a9c3c14b2724a4892a
Wind Lv <kaze_lvzf@outlook.com>
2019-08-20 18:18:37 +0800

    1152 complete

---
+++ 1152Google_Recruitment.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool isPrime(int num){
+	if(num==0||num==1) return false;
+	for(int i = 2; i * i <= num; i++)
+		if(num % i == 0) return false;
+	return true;
+}
+
+int main(){
+	int l, k;
+	string s;
+	cin >> l >> k >> s;
+
+	for(int i = 0 ; i <= l - k; i++){
+		string t = s.substr(i, k);
+		int num = stoi(t);
+
+		if(isPrime(num)){
+			cout << t;
+			return 0;
+		}
+	}
+	cout << "404" << endl;
+
+	return 0;
+}
